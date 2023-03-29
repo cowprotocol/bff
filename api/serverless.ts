629c05838796c61d11a351c27e65945cad03bf00
Tukan van der Borgh <tukanvdborgh@proton.me>
2023-03-29 16:57:34 +0200

    Fix type issue

--- api/serverless.ts
+++ api/serverless.ts
@@ -5,7 +5,7 @@ const app = fastify({ logger: true });
 
 app.register(App);
 
-export default async (req, res) => {
+export default async (req: any, res: any) => {
   await app.ready();
   app.server.emit("request", req, res);
 };
