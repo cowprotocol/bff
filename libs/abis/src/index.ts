b33bfadd42bacdbec8fffeb3678322c440dd241a
Tukan van der Borgh <tukanvdborgh@proton.me>
2023-07-06 13:25:09 +0200

    Remove unnecessary export

--- libs/abis/src/index.ts
+++ libs/abis/src/index.ts
@@ -1,4 +1,3 @@
 // Custom
 export * from './generated/custom';
 export type { GPv2Order } from './generated/custom/ComposableCoW';
-export { default as ComposableCoWAbi } from './abis/ComposableCoW.json';
