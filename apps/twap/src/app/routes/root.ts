1589b2800fa86f19f9fa60cb214f75d2c6ce3d68
Tukan van der Borgh <tukanvdborgh@proton.me>
2023-07-06 10:57:23 +0200

    Update root route

--- apps/twap/src/app/routes/root.ts
+++ apps/twap/src/app/routes/root.ts
@@ -2,9 +2,9 @@ import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
 
 export default async function (fastify: FastifyInstance) {
   fastify.get(
-    '/',
+    '/health-check',
     async function (request: FastifyRequest, reply: FastifyReply) {
-      return { message: 'Hello API' };
+      reply.status(200).send({ status: 'ok' });
     }
   );
 }
