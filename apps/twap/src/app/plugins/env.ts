16742ab59ca254f4876e18aa538d158b9f556e8f
Tukan van der Borgh <tukanvdborgh@proton.me>
2023-07-14 15:06:51 +0200

    feat(orderbook): :sparkles: add orderbook db info to env

--- apps/twap/src/app/plugins/env.ts
+++ apps/twap/src/app/plugins/env.ts
@@ -9,6 +9,10 @@ const schema = {
     'DATABASE_PASSWORD',
     'DATABASE_HOST',
     'DATABASE_PORT',
+    'ORDERBOOK_DATABASE_HOST',
+    'ORDERBOOK_DATABASE_PORT',
+    'ORDERBOOK_DATABASE_USERNAME',
+    'ORDERBOOK_DATABASE_PASSWORD',
   ],
   properties: {
     DATABASE_NAME: {
@@ -24,6 +28,18 @@ const schema = {
       type: 'string',
     },
     DATABASE_PASSWORD: {
+      type: 'string',
+    },
+    ORDERBOOK_DATABASE_HOST: {
+      type: 'string',
+    },
+    ORDERBOOK_DATABASE_PORT: {
+      type: 'number',
+    },
+    ORDERBOOK_DATABASE_USERNAME: {
+      type: 'string',
+    },
+    ORDERBOOK_DATABASE_PASSWORD: {
       type: 'string',
     },
   },
@@ -46,6 +62,10 @@ declare module 'fastify' {
       DATABASE_PASSWORD: string;
       DATABASE_HOST: string;
       DATABASE_PORT: number;
+      ORDERBOOK_DATABASE_HOST: string;
+      ORDERBOOK_DATABASE_PORT: number;
+      ORDERBOOK_DATABASE_USERNAME: string;
+      ORDERBOOK_DATABASE_PASSWORD: string;
     };
   }
 }
