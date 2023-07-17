5a86e8d7822b957aed63665f82308a549684e4c5
Tukan van der Borgh <tukanvdborgh@proton.me>
2023-07-17 14:45:14 +0200

    refactor(onchain-indexing): :recycle: add enum instead of inline values to api getter

--- apps/twap/src/app/utils/getApiBaseUrl.ts
+++ apps/twap/src/app/utils/getApiBaseUrl.ts
@@ -3,9 +3,9 @@ import { SupportedChainId } from '@cowprotocol/cow-sdk';
 const COW_API_BASE_URL = 'https://api.cow.fi';
 
 const CHAIN_ID_TO_NAME: Record<SupportedChainId, string> = {
-  1: 'mainnet',
-  5: 'goerli',
-  100: 'xdai',
+  [SupportedChainId.MAINNET]: 'mainnet',
+  [SupportedChainId.GOERLI]: 'goerli',
+  [SupportedChainId.GNOSIS_CHAIN]: 'xdai',
 };
 
 export function getApiBaseUrl(chainId: SupportedChainId): string {
