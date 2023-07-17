f74d2b72b15d5dfb1c24ad736313639e08cf1281
Tukan van der Borgh <tukanvdborgh@proton.me>
2023-07-17 14:38:54 +0200

    refactor(onchain-indexing): :recycle: refactor computeOrderUid to be more performant

--- apps/twap/src/app/utils/computeOrderUid.ts
+++ apps/twap/src/app/utils/computeOrderUid.ts
@@ -1,13 +1,27 @@
 import type { Order } from '@cowprotocol/contracts';
+import { computeOrderUid as _computeOrderUid } from '@cowprotocol/contracts';
 import {
   OrderParameters,
   OrderSigningUtils,
   SupportedChainId,
 } from '@cowprotocol/cow-sdk';
 import { returnNever } from '../types/order';
+import { TypedDataDomain } from 'ethers';
 
 function isOrder(order: Order | OrderParameters): order is Order {
   return true;
+}
+
+const domainCache: Partial<Record<SupportedChainId, TypedDataDomain>> = {};
+async function getDomain(chainId: SupportedChainId): Promise<TypedDataDomain> {
+  if (domainCache[chainId]) {
+    return domainCache[chainId];
+  }
+
+  const domain = await OrderSigningUtils.getDomain(chainId);
+  domainCache[chainId] = domain;
+
+  return domain;
 }
 
 export async function computeOrderUid(
@@ -15,10 +29,7 @@ export async function computeOrderUid(
   owner: string,
   order: Order | OrderParameters
 ): Promise<string> {
-  const { computeOrderUid: _computeOrderUid } = await import(
-    '@cowprotocol/contracts'
-  );
-  const domain = await OrderSigningUtils.getDomain(chainId);
+  const domain = await getDomain(chainId);
 
   if (isOrder(order)) {
     return _computeOrderUid(domain, order, owner);
