bdaa90e7acc0c1c2c12ff9d02cd2cc1e99806650
Anxo Rodriguez <anxolin@gmail.com>
2024-05-08 11:21:19 +0300

    Rename bff to api

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'bff',
+  displayName: 'api',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/bff',
+  coverageDirectory: '../../coverage/packages/api',
 };
