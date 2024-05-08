88cf8c7f778305195131d3c9349a6de22bd4a108
Anxo Rodriguez <anxolin@gmail.com>
2024-05-08 16:53:29 +0300

    Configure queue host and port

--- apps/telegram/src/main.ts
+++ apps/telegram/src/main.ts
@@ -44,65 +44,72 @@ const cmsClient = CmsClient({
 });
 
 // Connect to RabbitMQ server
-amqp.connect('amqp://localhost', function (error0, connection) {
-  if (error0) {
-    throw error0;
-  }
+const queueHost = process.env.QUEUE_HOST;
+assert(queueHost, 'QUEUE_HOST is required');
+const queuePort = process.env.QUEUE_PORT || 5672;
 
-  // Create a channel
-  connection.createChannel(function (error1, channel) {
-    if (error1) {
-      throw error1;
+amqp.connect(
+  'amqp://' + queueHost + ':' + queuePort,
+  function (error0, connection) {
+    if (error0) {
+      throw error0;
     }
 
-    // This makes sure the queue is declared
-    channel.assertQueue(NOTIFICATIONS_QUEUE, {
-      durable: false,
-    });
-
-    console.log(
-      '[telegram-consumer] Waiting for messages in %s',
-      NOTIFICATIONS_QUEUE
-    );
-
-    // Consume messages from RabbitMQ
-    channel.consume(
-      NOTIFICATIONS_QUEUE,
-      function (msg) {
-        const notification = parseNotification(msg.content.toString());
-        const { id, message, account, title, url } = notification;
-        console.log(
-          `[telegram-consumer] New Notification ${id} for ${account}. ${title}: ${message}. URL=${url}`
-        );
-
-        // TODO: Don't check every time!!
-        // Check in CMS if there's one
-        const subscriptionForAccount = cmsClient.GET(
-          `/tg-subscriptions/${account}`
-        ) as TelegramSubscription[];
-        subscriptions.set(account, subscriptionForAccount);
-
-        const telegramSubscriptions = subscriptions.get(account);
-        if (telegramSubscriptions.length > 0) {
-          // Send the message to all subscribers
-          for (const { chatId } of telegramSubscriptions) {
-            console.log(
-              `[telegram-consumer] Sending message ${id} to chatId ${chatId}`
-            );
-            bot.sendMessage(chatId, formatMessage(notification));
-          }
-        } else {
-          console.log(
-            `[telegram-consumer] No subscriptions found for account ${account}`
-          );
-        }
-      },
-      {
-        noAck: true,
+    // Create a channel
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
       }
-    );
-  });
-});
+
+      // This makes sure the queue is declared
+      channel.assertQueue(NOTIFICATIONS_QUEUE, {
+        durable: false,
+      });
+
+      console.log(
+        '[telegram-consumer] Waiting for messages in %s',
+        NOTIFICATIONS_QUEUE
+      );
+
+      // Consume messages from RabbitMQ
+      channel.consume(
+        NOTIFICATIONS_QUEUE,
+        function (msg) {
+          const notification = parseNotification(msg.content.toString());
+          const { id, message, account, title, url } = notification;
+          console.log(
+            `[telegram-consumer] New Notification ${id} for ${account}. ${title}: ${message}. URL=${url}`
+          );
+
+          // TODO: Don't check every time!!
+          // Check in CMS if there's one
+          const subscriptionForAccount = cmsClient.GET(
+            `/tg-subscriptions/${account}`
+          ) as TelegramSubscription[];
+          subscriptions.set(account, subscriptionForAccount);
+
+          const telegramSubscriptions = subscriptions.get(account);
+          if (telegramSubscriptions.length > 0) {
+            // Send the message to all subscribers
+            for (const { chatId } of telegramSubscriptions) {
+              console.log(
+                `[telegram-consumer] Sending message ${id} to chatId ${chatId}`
+              );
+              bot.sendMessage(chatId, formatMessage(notification));
+            }
+          } else {
+            console.log(
+              `[telegram-consumer] No subscriptions found for account ${account}`
+            );
+          }
+        },
+        {
+          noAck: true,
+        }
+      );
+    });
+  }
+);
 
 function formatMessage({ title, message, url }: Notification) {
   return `\
