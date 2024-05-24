5f9d9e0cb728f426db3a4e9b4432428d4d78e33f
Anxo Rodriguez <anxolin@gmail.com>
2024-05-24 16:46:37 +0100

    Expose commit hash and version in the about service

--- apps/api/src/app/routes/about/index.ts
+++ apps/api/src/app/routes/about/index.ts
@@ -1,18 +1,39 @@
+import { log } from 'console';
 import { FastifyPluginAsync } from 'fastify';
-import { version } from '../../../../../../package.json';
+
+import { readFileSync } from 'fs';
+const GIT_COMMIT_HASH_FILE = '.git-commit-hash';
+const VERSION = process.env.VERSION || 'UNKNOWN, please set the environment variable';
+const COMMIT_HASH = getCommitHash();
+
 
 interface AboutResponse {
   name: string;
   version: string;
+  commitHash?: string | undefined;
 }
 
 const example: FastifyPluginAsync = async (fastify): Promise<void> => {
   fastify.get<{ Reply: AboutResponse }>('/', async function (_request, reply) {
     return reply.send({
       name: 'BFF API',
-      version,
+      version: VERSION,
+      commitHash: COMMIT_HASH,
     });
   });
 };
+
+/**
+ * Read a file with the git commit hash (generated for example using github actions)
+ */
+function getCommitHash(): string | undefined {
+  try {
+    return readFileSync(GIT_COMMIT_HASH_FILE, 'utf-8')
+  } catch (error) {
+    // Not a big deal, if the file is not present, the about won't 
+    console.warn(`Unable to read the commit hash file: ${GIT_COMMIT_HASH_FILE}. It won't be exported in the about section`);  
+    return undefined
+  }
+}
 
 export default example;
