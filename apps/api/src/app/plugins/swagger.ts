c1cca1e8ed9c35ecce5a2d9d0cef6cf0160c37e6
Anxo Rodriguez <anxolin@gmail.com>
2024-05-28 17:48:00 +0100

    Hide some routes in swagger (#34)

--- apps/api/src/app/plugins/swagger.ts
+++ apps/api/src/app/plugins/swagger.ts
@@ -3,9 +3,21 @@ import fp from 'fastify-plugin';
 import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUi from '@fastify/swagger-ui';
 
+const HIDDEN_PATHS = ['/'];
+const HIDDEN_BASE_PATHS = ['/proxies', '/proxy', '/twap'];
+
 export default fp(async function (fastify: FastifyInstance) {
   fastify.register(fastifySwagger, {
-    swagger: {},
+    swagger: {
+    },
+    transform: (param) => {
+      const { schema = {}, url } = param
+
+      // can add the hide tag if needed
+      const modifiedSchema = (isHiddenPath(url)) ? { ...schema, hide: true } : schema
+
+      return { ...param, schema: modifiedSchema }
+    }
   });
   fastify.register(fastifySwaggerUi, {
     routePrefix: '/docs',
@@ -14,4 +26,17 @@ export default fp(async function (fastify: FastifyInstance) {
     if (err) throw err;
     fastify.swagger();
   });
+
 });
+
+function isHiddenPath(url: string) {
+  if (HIDDEN_BASE_PATHS.some(basePath => url.startsWith(basePath))) {
+    return true;
+  }
+
+  if (HIDDEN_PATHS.some(basePath => url === basePath)) {
+    return true;
+  }
+
+  return false
+}
