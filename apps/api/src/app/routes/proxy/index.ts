639f01939aa2abbc7fd96874e69cae8a4d1c3eb0
Anxo Rodriguez <anxolin@gmail.com>
2024-06-06 14:33:32 +0100

    Delete legacy proxy endpoint

--- apps/api/src/app/routes/proxy/index.ts
+++
@@ -1,5 +0,0 @@
-import tokensProxy from "../proxies/tokens";
-
-// TODO: temporarily re-export old path while we migrate deployed services
-// TODO: remove this path after 2 months: by 2024-07-01
-export default tokensProxy;
