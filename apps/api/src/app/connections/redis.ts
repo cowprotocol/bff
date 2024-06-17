4cf97a8f15dcd7b8bec7c3d3cad8bacd84a49893
Anxo Rodriguez <anxolin@gmail.com>
2024-06-17 14:54:13 +0200

    Add credentials

--- apps/api/src/app/connections/redis.ts
+++ apps/api/src/app/connections/redis.ts
@@ -3,5 +3,7 @@ import IORedis from 'ioredis';
 export const redis = new IORedis({
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: parseInt(process.env.REDIS_PORT || '6379', 10),
-  db: 0
+  db: 0,
+  username: process.env.REDIS_USER,
+  password: process.env.REDIS_PASSWORD,
 })
