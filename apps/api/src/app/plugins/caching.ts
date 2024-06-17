0446f128952012363e1d2f690aac246037db4958
Anxo Rodriguez <anxolin@gmail.com>
2024-06-17 17:57:50 +0200

    force import of abstract-cache-redis

--- apps/api/src/app/plugins/caching.ts
+++ apps/api/src/app/plugins/caching.ts
@@ -2,6 +2,7 @@ import fastifyCaching, { FastifyCachingPluginOptions } from '@fastify/caching';
 import fp from "fastify-plugin";
 import abstractCache from 'abstract-cache'
 import { redis } from '../connections/redis';
+import 'abstract-cache-redis';
 
 
 const isRedisEnabled = !!process.env.REDIS_HOST;
