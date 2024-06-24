c3f4af7eb0c5b3461a8e4d704af1062cef6a2b09
Anxo Rodriguez <anxolin@gmail.com>
2024-06-24 17:19:36 +0100

    Add a simple authentication based on authorized domains (#39)
    * Add a simple authentication based on authorized domains
    
    * Add auth layer
    
    * Fix some issues after test
    
    * Make use of strict equality
    
    Co-authored-by: Leandro <alfetopito@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Leandro <alfetopito@users.noreply.github.com>

---
+++ apps/api/src/app/plugins/bffAuth.ts
@@ -0,0 +1,54 @@
+import fp from "fastify-plugin";
+import { FastifyPluginCallback } from "fastify";
+
+const PROTECTED_PATHS = ['/proxies']
+
+const AUTHORIZED_ORIGINS = (() => {
+  const domains = process.env.AUTHORIZED_ORIGINS
+  if (!domains) {
+    return []
+  }
+
+  return domains.split(',').map(domain => domain.trim())
+})()
+
+
+export const bffAuth: FastifyPluginCallback = (fastify, opts, next) => {
+  fastify.addHook('onRequest', async (request, reply) => {
+    // Return early if its an unprotected path
+    if (AUTHORIZED_ORIGINS.length === 0 || !PROTECTED_PATHS.some(path => request.url.startsWith(path))) {
+      return
+    }
+
+    const origin = request.headers.origin
+
+    // Check the origin
+    if (
+      // Origin should be present
+      !origin ||
+      (
+        // The origin should be explicitly authorized
+        !AUTHORIZED_ORIGINS.some(authorizedOrigin => origin.endsWith(authorizedOrigin)) &&
+
+        // Make an exception for localhost
+        !isLocalhost(origin)
+      )
+    ) {
+      reply.status(403).send('Unauthorized')
+      return
+    }
+
+    return
+  })
+
+  next()
+}
+
+function isLocalhost(origin: string): boolean {
+  if (!origin) {
+    return false
+  }
+  return /^http:\/\/localhost:\d+\/?$/.test(origin)
+}
+
+export default fp(bffAuth, { fastify: '4.x', name: 'bffAuth' })
