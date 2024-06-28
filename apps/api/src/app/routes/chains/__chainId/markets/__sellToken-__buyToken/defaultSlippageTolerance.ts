7aa65fcd4da271f29a19ad1b6540d0dc2aa820ef
Anxo Rodriguez <anxolin@gmail.com>
2024-06-28 18:33:17 +0100

    Add default slippage endpoint

---
+++ apps/api/src/app/routes/chains/__chainId/markets/__sellToken-__buyToken/defaultSlippageTolerance.ts
@@ -0,0 +1,48 @@
+import { ChainIdSchema, ETHEREUM_ADDRESS_PATTERN } from '../../../../../schemas';
+import { FastifyPluginAsync } from 'fastify';
+import { FromSchema, JSONSchema } from 'json-schema-to-ts';
+
+interface Result {
+  slippagePercent: number;
+}
+
+const routeSchema = {
+  type: 'object',
+  required: ['chainId', 'sellToken', 'buyToken'],
+  properties: {
+    chainId: ChainIdSchema,
+    sellToken: {
+      title: 'Sell token address',
+      description: 'Sell token address',
+      type: "string",
+      pattern: ETHEREUM_ADDRESS_PATTERN
+    },
+    buyToken: {
+      title: 'Buy token address',
+      description: 'Buy token address',
+      type: 'string',
+      pattern: ETHEREUM_ADDRESS_PATTERN
+    },
+  },
+} as const satisfies JSONSchema;
+
+
+type RouteSchema = FromSchema<typeof routeSchema>;
+
+
+const root: FastifyPluginAsync = async (fastify): Promise<void> => {
+  // example: http://localhost:3010/chains/1/markets/0x6b175474e89094c44da98b954eedeac495271d0f-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599/defaultSlippageTolerance
+
+  fastify.get<{
+    Params: RouteSchema;
+    Reply: Result;
+  }>('/defaultSlippageTolerance', {
+    schema: { params: routeSchema }
+  }, async function (request, reply) {
+    const { chainId, sellTokenAddress, buyTokenAddress } = request.params;
+    fastify.log.info(`Get default slippage for market ${sellTokenAddress}-${buyTokenAddress} on chain ${chainId}`);
+    reply.send({ slippagePercent: 0.5 })
+  });
+};
+
+export default root;
