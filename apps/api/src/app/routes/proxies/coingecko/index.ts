17a5c2e5a142c7595e8a717de741b3fc073cdb6d
Alfetopito <alfetopito@users.noreply.github.com>
2024-07-01 11:36:08 +0100

    refactor: style changes

--- apps/api/src/app/routes/proxies/coingecko/index.ts
+++ apps/api/src/app/routes/proxies/coingecko/index.ts
@@ -1,53 +1,57 @@
-import httpProxy from "@fastify/http-proxy";
-import { CACHE_CONTROL_HEADER, getCacheControlHeaderValue } from "../../../../utils/cache";
-import { FastifyPluginAsync } from "fastify";
+import httpProxy from '@fastify/http-proxy'
+import { FastifyPluginAsync } from 'fastify'
+import { CACHE_CONTROL_HEADER, getCacheControlHeaderValue } from '../../../../utils/cache'
 
 const DROP_HEADERS = ['cf-ray', 'cf-cache-status', 'set-cookie', 'server']
 
 const CACHE_TTL = parseInt(process.env.COINGECKO_CACHING_TIME || '150') // Defaults to 2.5 minutes (150 seconds)
-const DEFAULT_COINGECKO_PROXY_UPSTREAM = "https://api.coingecko.com";
+const DEFAULT_COINGECKO_PROXY_UPSTREAM = 'https://api.coingecko.com'
 
 const coingeckoProxy: FastifyPluginAsync = async (
   fastify,
-  opts
+  _opts
 ): Promise<void> => {
   fastify.register(httpProxy, {
     upstream:
       fastify.config.COINGECKO_PROXY_UPSTREAM ||
       DEFAULT_COINGECKO_PROXY_UPSTREAM,
-    rewritePrefix: "/api/v3/",
+    rewritePrefix: '/api/v3/',
     replyOptions: {
       rewriteRequestHeaders: (request, headers) => ({
         ...headers,
-        "x-cg-pro-api-key": fastify.config.COINGECKO_API_KEY,
+        'x-cg-pro-api-key': fastify.config.COINGECKO_API_KEY
       }),
       // Response headers https://github.com/fastify/fastify-reply-from?tab=readme-ov-file#rewriteheadersheaders-request
-      rewriteHeaders: (headers, request) => {
-
+      rewriteHeaders: (headers, _request) => {
         // Drop some headers
-        const newHeaders = DROP_HEADERS.reduce((acc, header) => {
-          delete acc[header];
-          return acc;
-        }, {
-          ...headers,
-          ...(headers[CACHE_CONTROL_HEADER]
-            ? {
-              [CACHE_CONTROL_HEADER]: getCacheControlHeaderValue(CACHE_TTL),
-            }
-            : undefined)
-        });
+        const newHeaders = DROP_HEADERS.reduce(
+          (acc, header) => {
+            delete acc[header]
+            return acc
+          },
+          {
+            ...headers,
+            ...(headers[CACHE_CONTROL_HEADER]
+              ? {
+                [CACHE_CONTROL_HEADER]: getCacheControlHeaderValue(CACHE_TTL)
+              }
+              : undefined)
+          }
+        )
 
         return newHeaders
-
-      },
+      }
     },
     preHandler: async (request) => {
-      fastify.log.debug({ url: request.url, method: request.method }, `Request coingecko proxy`);
+      fastify.log.debug(
+        { url: request.url, method: request.method },
+        `Request coingecko proxy`
+      )
     },
     undici: {
-      strictContentLength: false, // Prevent errors when content-length header mismatches
-    },
-  });
-};
+      strictContentLength: false // Prevent errors when content-length header mismatches
+    }
+  })
+}
 
-export default coingeckoProxy;
+export default coingeckoProxy
