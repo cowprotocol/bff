07f91e2de020dcae89fd428c9a2df3d255ea07ea
Anxo Rodriguez <anxolin@gmail.com>
2024-07-18 12:12:19 +0100

    Implement proxy

--- libs/repositories/src/UsdRepository/UsdRepositoryRedis.ts
+++ libs/repositories/src/UsdRepository/UsdRepositoryRedis.ts
@@ -1,40 +1,22 @@
 import { injectable } from 'inversify';
-import {
-  PricePoint,
-  PriceStrategy,
-  UsdRepository,
-  UsdRepositoryNoop,
-} from './UsdRepository';
-import { cowApiClientMainnet } from '../cowApi';
-import { OneBigNumber, TenBigNumber, USDC, ZeroBigNumber } from '../const';
+import { PricePoint, PriceStrategy, UsdRepository } from './UsdRepository';
 import { SupportedChainId } from '../types';
-import { BigNumber } from 'bignumber.js';
-import { throwIfUnsuccessful } from '../utils/throwIfUnsuccessful';
 
 @injectable()
 export class UsdRepositoryRedis implements UsdRepository {
+  constructor(private proxy: UsdRepository) {}
+
   getUsdPrice(
     chainId: SupportedChainId,
     tokenAddress: string
   ): Promise<number | null> {
-    throw new Error('Method not implemented.');
+    return this.proxy.getUsdPrice(chainId, tokenAddress);
   }
   getUsdPrices(
     chainId: SupportedChainId,
     tokenAddress: string,
     priceStrategy: PriceStrategy
   ): Promise<PricePoint[] | null> {
-    throw new Error('Method not implemented.');
+    return this.proxy.getUsdPrices(chainId, tokenAddress, priceStrategy);
   }
-}
-
-/**
- * API response value represents the amount of native token atoms needed to buy 1 atom of the specified token
- * This function inverts the price to represent the amount of specified token atoms needed to buy 1 atom of the native token
- */
-function invertNativeToTokenPrice(
-  value: BigNumber,
-  decimals: number
-): BigNumber {
-  return OneBigNumber.times(TenBigNumber.pow(18 - decimals)).div(value);
 }
