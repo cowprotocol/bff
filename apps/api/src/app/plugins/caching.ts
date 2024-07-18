bdc216e295847c058b0e01f56d2ff36b0d73e93d
Anxo Rodriguez <anxolin@gmail.com>
2024-07-18 12:06:07 +0100

    Move redis client to repositories layer

--- apps/api/src/app/plugins/caching.ts
+++ apps/api/src/app/plugins/caching.ts
@@ -1,28 +1,26 @@
 import fastifyCaching, { FastifyCachingPluginOptions } from '@fastify/caching';
-import fp from "fastify-plugin";
-import abstractCache from 'abstract-cache'
-import { redis } from '../connections/redis';
+import fp from 'fastify-plugin';
+import abstractCache from 'abstract-cache';
+import { redisClient } from '../../../../../libs/repositories/src/datasources/redis';
 import 'abstract-cache-redis';
-
-
 
 export default fp(async (fastify, opts) => {
   const options: FastifyCachingPluginOptions = {
     ...opts,
 
-    ...(redis ? {
-      cache: abstractCache({
-        useAwait: false,
-        driver: {
-          name: 'abstract-cache-redis',
-          options: {
-            client: redis
-          }
+    ...(redisClient
+      ? {
+          cache: abstractCache({
+            useAwait: false,
+            driver: {
+              name: 'abstract-cache-redis',
+              options: {
+                client: redisClient,
+              },
+            },
+          }),
         }
-      })
-    } : {})
+      : {}),
   };
   fastify.register(fastifyCaching, options);
 });
-
-
