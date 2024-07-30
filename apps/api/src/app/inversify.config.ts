196d8fd49fed037f66b438ae09f9ed4a2338a249
Anxo Rodriguez <anxolin@gmail.com>
2024-07-30 16:53:08 +0100

    Fix grammatical error

--- apps/api/src/app/inversify.config.ts
+++ apps/api/src/app/inversify.config.ts
@@ -19,7 +19,7 @@ import {
 } from '@cowprotocol/repositories';
 
 const DEFAULT_CACHE_VALUE_SECONDS = ms('2min') / 1000; // 2min cache time by default for values
-const DEFAULT_CACHE_NULL_SECONDS = ms('30min') / 1000; // 30min cache time by default for NULL values (when the repository don't know)
+const DEFAULT_CACHE_NULL_SECONDS = ms('30min') / 1000; // 30min cache time by default for NULL values (when the repository isn't known)
 
 const CACHE_TOKEN_INFO_SECONDS = ms('24h') / 1000; // 24h
 
