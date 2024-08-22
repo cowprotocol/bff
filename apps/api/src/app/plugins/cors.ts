fd118367f64ea9436c0caf2599db708e31709008
Anxo Rodriguez <anxolin@gmail.com>
2024-08-22 16:14:01 +0100

    Make it easier to test in local

--- apps/api/src/app/plugins/cors.ts
+++ apps/api/src/app/plugins/cors.ts
@@ -1,10 +1,25 @@
-import cors from "@fastify/cors";
-import fp from "fastify-plugin";
+import cors, { FastifyCorsOptions } from '@fastify/cors';
+import fp from 'fastify-plugin';
 
 export default fp(async (fastify, opts) => {
-  const options = {
+  const options: FastifyCorsOptions = {
     ...opts,
     origin: false,
+    delegator: (req, callback) => {
+      const corsOptions = {
+        origin: false,
+      };
+
+      // do not include CORS headers for requests from localhost
+      const origin = req.headers.origin;
+      console.log('delegator', origin);
+      if (origin && /^http:\/\/localhost/.test(origin)) {
+        corsOptions.origin = true;
+      }
+
+      // callback expects two parameters: error and options
+      callback(null, corsOptions);
+    },
   };
   fastify.register(cors, options);
 });
