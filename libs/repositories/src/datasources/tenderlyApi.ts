78d10ded6d9e946d0d24fc052f35fb0ee91f5734
Pedro Yves Fracari <55461956+yvesfracari@users.noreply.github.com>
2024-10-31 13:24:43 -0300

    fix: add public link on tenderly simulations (#95)

--- libs/repositories/src/datasources/tenderlyApi.ts
+++ libs/repositories/src/datasources/tenderlyApi.ts
@@ -6,5 +6,6 @@ export const TENDERLY_PROJECT_NAME = process.env.TENDERLY_PROJECT_NAME;
 export const TENDERLY_API_BASE_ENDPOINT = `https://api.tenderly.co/api/v1/account/${TENDERLY_ORG_NAME}/project/${TENDERLY_PROJECT_NAME}`;
 
 export const getTenderlySimulationLink = (simulationId: string): string => {
-  return `https://dashboard.tenderly.co/${TENDERLY_ORG_NAME}/${TENDERLY_PROJECT_NAME}/simulator/${simulationId}`;
+  // This link only work for public projects, if the project is private remove the "/public" from the link
+  return `https://dashboard.tenderly.co/public/${TENDERLY_ORG_NAME}/${TENDERLY_PROJECT_NAME}/simulator/${simulationId}`;
 };
