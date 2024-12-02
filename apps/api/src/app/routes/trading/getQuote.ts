af371ad5fe7d605718787a02703500a1968c8bd2
shoom3301 <shoom3301@gmail.com>
2024-12-02 21:47:31 +0500

    chore: rename quote endpoint

--- apps/api/src/app/routes/trading/getQuote.ts
+++ apps/api/src/app/routes/trading/getQuote.ts
@@ -24,7 +24,7 @@ const root: FastifyPluginAsync = async (fastify): Promise<void> => {
     Reply: SuccessSchema | ErrorSchema;
     Body: BodySchema;
   }>(
-    '/quote',
+    '/quote-requests',
     {
       schema: {
         body: getQuoteBodySchema,
