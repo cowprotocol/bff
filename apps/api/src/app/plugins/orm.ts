503e839939388e6c3d30737dfd2c935ff25858a2
shoom3301 <shoom3301@gmail.com>
2024-12-03 14:21:04 +0500

    chore: do not fail when cow  analytics db vars are not set

--- apps/api/src/app/plugins/orm.ts
+++ apps/api/src/app/plugins/orm.ts
@@ -5,13 +5,24 @@ import fp from 'fastify-plugin';
 import { PoolInfo } from '../data/poolInfo';
 
 export default fp(async function (fastify: FastifyInstance) {
-  fastify.register(typeORMPlugin, {
+  const dbParams = {
     host: fastify.config.COW_ANALYTICS_DATABASE_HOST,
     port: Number(fastify.config.COW_ANALYTICS_DATABASE_PORT),
-    type: 'postgres',
     database: fastify.config.COW_ANALYTICS_DATABASE_NAME,
     username: fastify.config.COW_ANALYTICS_DATABASE_USERNAME,
     password: fastify.config.COW_ANALYTICS_DATABASE_PASSWORD,
+  }
+
+  const dbParamsAreInvalid = Object.values(dbParams).some((v) => Number.isNaN(v) || v === undefined);
+
+  if (dbParamsAreInvalid) {
+    console.error('Invalid CoW Analytics database parameters, please check COW_ANALYTICS_* env vars');
+    return
+  }
+
+  fastify.register(typeORMPlugin, {
+    ...dbParams,
+    type: 'postgres',
     entities: [PoolInfo],
     ssl: true,
     extra: {
