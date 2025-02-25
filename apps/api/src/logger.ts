3243e74deb9f20bda2bab9e4027d40f11dae53c0
Anxo Rodriguez <anxolin@gmail.com>
2025-02-25 22:07:14 +0000

    chore: changen env name to LOG_FORMAT

--- apps/api/src/logger.ts
+++ apps/api/src/logger.ts
@@ -3,10 +3,10 @@ import pino from 'pino';
 export type Logger = pino.Logger;
 
 function getLogger() {
-  // Uses pretty print if LOGGER is set to 'pretty'. By default, it will also use it for non-production environments.
-  // If the LOGGER is not pretty, it defaults to a JSON logger.
-  const usePrettyPrint = process.env.LOGGER
-    ? process.env.LOGGER === 'pretty'
+  // Uses pretty print if env.LOG_FORMAT is set to 'pretty'. By default, it will also use it for non-production environments.
+  // If the env.LOG_FORMAT is not 'pretty', it defaults to a JSON logger.
+  const usePrettyPrint = process.env.LOG_FORMAT
+    ? process.env.LOG_FORMAT === 'pretty'
     : process.env.NODE_ENV !== 'production';
 
   const loggerConfigEnv = usePrettyPrint
