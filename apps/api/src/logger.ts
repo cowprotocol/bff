bf55b2b1dc14e2028acaacd2d2eedadba54d7582
Anxo Rodriguez <anxolin@gmail.com>
2025-02-25 22:00:26 +0000

    feat: allow to pretty-print in production

--- apps/api/src/logger.ts
+++ apps/api/src/logger.ts
@@ -2,16 +2,25 @@ import pino from 'pino';
 
 export type Logger = pino.Logger;
 
-const loggerConfigEnv =
-  process.env.NODE_ENV === 'production'
-    ? {}
-    : {
+function getLogger() {
+  // Uses pretty print if LOGGER is set to 'pretty'. By default, it will also use it for non-production environments.
+  // If the LOGGER is not pretty, it defaults to a JSON logger.
+  const usePrettyPrint = process.env.LOGGER
+    ? process.env.LOGGER === 'pretty'
+    : process.env.NODE_ENV !== 'production';
+
+  const loggerConfigEnv = usePrettyPrint
+    ? {
         transport: {
           target: 'pino-pretty',
         },
-      };
+      }
+    : {};
 
-export const logger = pino({
-  ...loggerConfigEnv,
-  level: process.env.LOG_LEVEL ?? 'info',
-});
+  return pino({
+    ...loggerConfigEnv,
+    level: process.env.LOG_LEVEL ?? 'info',
+  });
+}
+
+export const logger = getLogger();
