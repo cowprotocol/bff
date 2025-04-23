2f7148340964fb6c2b2b8e95546d17e08f24a2f5
Anxo Rodriguez <anxolin@gmail.com>
2025-04-23 20:01:54 +0100

    chore: add automatic migrations for API

---
+++ apps/api/src/app/plugins/orm-repositories.ts
@@ -0,0 +1,67 @@
+import 'reflect-metadata';
+import { FastifyInstance } from 'fastify';
+import typeORMPlugin from 'typeorm-fastify-plugin';
+import fp from 'fastify-plugin';
+import { resolve } from 'path';
+import { logger } from '@cowprotocol/shared';
+
+import { getDatabaseParams } from '@cowprotocol/repositories';
+import { readdir } from 'fs/promises';
+
+export default fp(async function (fastify: FastifyInstance) {
+  const isProduction = process.env.NODE_ENV === 'production';
+
+  const migrationsDir = resolve(
+    __dirname,
+    '../../../../../libs/repositories/src/migrations'
+  );
+
+  fastify.register(typeORMPlugin, {
+    ...getDatabaseParams(),
+    namespace: 'repositories',
+    type: 'postgres',
+    migrations: [`${migrationsDir}/*.js`],
+    migrationsTableName: 'migrations_repositories',
+    entities: [],
+    ssl: isProduction,
+    extra: isProduction
+      ? {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        }
+      : undefined,
+    logging: true, // Enable TypeORM logging
+  });
+  fastify.ready(async (err) => {
+    if (err) {
+      throw err;
+    }
+
+    try {
+      logger.info('Starting migrations...');
+      logger.info('Migrations dir:', migrationsDir);
+      printMigrations(migrationsDir);
+      const result = await fastify.orm.repositories.runMigrations({
+        transaction: 'all',
+      });
+      logger.info(`${result.length} migrations applied`);
+    } catch (error) {
+      logger.error('Migration error:', error);
+      throw error;
+    }
+  });
+});
+
+async function printMigrations(dirPath: string) {
+  try {
+    const files = (await readdir(dirPath)).filter((file) =>
+      file.endsWith('.js')
+    );
+    for (const file of files) {
+      logger.info(`  - ${file}`);
+    }
+  } catch (err) {
+    logger.error('Error reading directory:', err);
+  }
+}
--- libs/repositories/src/datasources/orm/postgresOrm.ts
+++ libs/repositories/src/datasources/orm/postgresOrm.ts
@@ -2,9 +2,8 @@
 import { DataSource } from 'typeorm';
 
 import assert from 'assert';
-import { IndexerState } from '../../database/IndexerState.entity';
 
-export function createNewPostgresOrm(): DataSource {
+export function getDatabaseParams() {
   // Note: not using the `ensureEnvs` util function because it causes issues with the migrations)
   assert(process.env.DATABASE_HOST, 'DATABASE_HOST is not set');
   assert(process.env.DATABASE_PORT, 'DATABASE_PORT is not set');
@@ -12,15 +11,19 @@ export function createNewPostgresOrm(): DataSource {
   assert(process.env.DATABASE_PASSWORD, 'DATABASE_PASSWORD is not set');
   assert(process.env.DATABASE_NAME, 'DATABASE_NAME is not set');
 
-  const dataSource = new DataSource({
-    type: 'postgres',
+  return {
     host: process.env.DATABASE_HOST,
     port: Number(process.env.DATABASE_PORT),
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    // entities: ['src/database/*.ts'],
-    entities: [IndexerState],
+  };
+}
+
+export function createNewPostgresOrm(): DataSource {
+  const dataSource = new DataSource({
+    type: 'postgres',
+    ...getDatabaseParams(),
     migrations: ['src/migrations/*.ts'],
     migrationsTableName: 'migrations_repositories',
   });
