4d1140bbeed65f92e319a36b81e5392b4d0691ba
Alfetopito <alfetopito@users.noreply.github.com>
2025-08-05 17:03:24 +0100

    feat: add lens/bnb config to data sources

--- libs/repositories/src/datasources/ethplorer.ts
+++ libs/repositories/src/datasources/ethplorer.ts
@@ -2,6 +2,9 @@ import { SupportedChainId } from '@cowprotocol/cow-sdk';
 
 export const ETHPLORER_API_KEY = process.env.ETHPLORER_API_KEY as string;
 
+/**
+ * From https://github.com/EverexIO/Ethplorer/wiki/Ethplorer-API#supported-chains
+ */
 export const ETHPLORER_BASE_URL: Record<SupportedChainId, string | null> = {
   [SupportedChainId.MAINNET]: 'https://api.ethplorer.io',
   [SupportedChainId.SEPOLIA]: 'https://sepolia-api.ethplorer.io',
@@ -10,4 +13,6 @@ export const ETHPLORER_BASE_URL: Record<SupportedChainId, string | null> = {
   [SupportedChainId.BASE]: null,
   [SupportedChainId.POLYGON]: null,
   [SupportedChainId.AVALANCHE]: null,
+  [SupportedChainId.LENS]: null,
+  [SupportedChainId.BNB]: 'https://api.binplorer.com',
 };
