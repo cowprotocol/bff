9dad667f9e9667ebb3f8842c4e4b187e8ffba329
Anxo Rodriguez <anxolin@gmail.com>
2025-09-03 18:56:25 +0100

    feat: add sse service (#158)
    * feat: add sse service
    
    * chore: define const

--- libs/services/src/index.ts
+++ libs/services/src/index.ts
@@ -7,4 +7,8 @@ export * from './UsdService/UsdService';
 export * from './TokenHolderService/TokenHolderService';
 
 export * from './SimulationService/SimulationService';
+
+export * from './SSEService/SSEService';
+export * from './SSEService/SSEServiceMain';
+
 export * from './factories';
