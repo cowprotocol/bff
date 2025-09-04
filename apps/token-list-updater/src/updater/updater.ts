cddf7366c8cc2f5c73328c628b7b8d295fc97661
daniele <daniele@cow.fi>
2025-09-04 12:06:21 +0100

    feat: add token-list-updater app

---
+++ apps/token-list-updater/src/updater/updater.ts
@@ -0,0 +1,55 @@
+import { initTokenList, TokenCacheRepository } from '@cowprotocol/repositories';
+import { logger, doForever } from '@cowprotocol/shared';
+import { Runnable } from '../../types';
+
+const WAIT_TIME = 1000 * 60 * 60 * 6; // 6 hours
+
+export type TokenListUpdaterProps = {
+  chainId: number;
+  tokenCacheRepository: TokenCacheRepository;
+};
+
+export class TokenListUpdater implements Runnable {
+  isStopping = false;
+
+  constructor(private props: TokenListUpdaterProps) {}
+
+  /**
+   * Main loop: Run the token list updater. This method runs indefinitely,
+   * updating the token list for a specific chain.
+   *
+   * The method should not throw or finish.
+   */
+  async start(): Promise<void> {
+    await doForever({
+      name: `TokenListUpdater for chain id: ${this.props.chainId}`,
+      callback: async (stop) => {
+        if (this.isStopping) {
+          stop();
+          return;
+        }
+        logger.info(`Updating token list for chain id: ${this.props.chainId}`);
+        await this.updateTokenList();
+      },
+      waitTimeMilliseconds: WAIT_TIME,
+      logger,
+    });
+
+    logger.info(
+      `TokenListUpdater for chain id: ${this.props.chainId}`,
+      'stopped'
+    );
+  }
+
+  async stop(): Promise<void> {
+    this.isStopping = true;
+  }
+
+  async updateTokenList(): Promise<void> {
+    await initTokenList(this.props.chainId, this.props.tokenCacheRepository);
+
+    logger.debug(
+      `[tokenlist-updater:main] Token list updated for chain id: ${this.props.chainId}`
+    );
+  }
+}
