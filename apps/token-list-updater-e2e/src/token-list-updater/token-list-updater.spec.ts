0ced8ecbe46dbdf7ef6ccea0f9881fe6d165bd5a
daniele <daniele@cow.fi>
2025-09-09 11:19:29 +0100

    chore: skip wrong test and add comment

--- apps/token-list-updater-e2e/src/token-list-updater/token-list-updater.spec.ts
+++ apps/token-list-updater-e2e/src/token-list-updater/token-list-updater.spec.ts
@@ -2,7 +2,8 @@ import { execSync } from 'child_process';
 import { join } from 'path';
 
 describe('CLI tests', () => {
-  it('should print a message', () => {
+  // TODO: implement properly this test
+  it.skip('should print a message', () => {
     const cliPath = join(process.cwd(), 'dist/apps/token-list-updater');
 
     const output = execSync(`node ${cliPath}`).toString();
