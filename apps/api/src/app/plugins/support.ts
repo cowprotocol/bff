c61f148b464ebbcbda4f71cbb7c4ffcc81a8b317
Alexandr Kazachenko <shoom3301@gmail.com>
2025-10-13 13:36:59 +0500

    ci: add test and lint GH action (#172)
    * chore: automatic update of generated types
    
    * feat: add getVolatilityForPair
    
    * refactor: sort imports
    
    * feat: replace max volatility with relative
    
    * feat: return 0 when volatility is unknown
    
    * feat: return volatility as is, without min or max
    
    * chore: WIP, revert
    
    * chore: fix merge
    
    * chore: disable integration tests
    
    * chore: remove logs
    
    * chore: fix slippage volatile
    
    * chore: fix flaky test
    
    * ci: fix lint scripts
    
    * ci: add test and lint GH action
    
    * ci: hardcode GH action libs versions
    
    * test: add unit-tests for getCoingeckoProClient
    
    * chore: fix test-and-lint trigger
    
    * chore: fix if-no-files-found
    
    * chore: disable integration tests
    
    ---------
    
    Co-authored-by: Alfetopito <alfetopito@users.noreply.github.com>

--- apps/api/src/app/plugins/support.ts
+++ apps/api/src/app/plugins/support.ts
@@ -1,12 +1,8 @@
 import fp from 'fastify-plugin'
 
-export interface SupportPluginOptions {
-  // Specify Support plugin options here
-}
-
 // The use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
-export default fp<SupportPluginOptions>(async (fastify, opts) => {
+export default fp(async (fastify) => {
   fastify.decorate('someSupport', function () {
     return 'hugs'
   })
