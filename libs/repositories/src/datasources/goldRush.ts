c129c252351ba2c1d01b63c151219cd21b83a97b
Alfetopito <alfetopito@users.noreply.github.com>
2025-10-28 11:12:44 +0000

    feat: add linea/plasma goldrush settings

--- libs/repositories/src/datasources/goldRush.ts
+++ libs/repositories/src/datasources/goldRush.ts
@@ -8,7 +8,7 @@ export const GOLD_RUSH_API_BASE_URL = 'https://api.covalenthq.com';
  */
 export const GOLD_RUSH_CLIENT_NETWORK_MAPPING: Record<
   SupportedChainId,
-  string
+  string | null
 > = {
   [SupportedChainId.MAINNET]: 'eth-mainnet',
   [SupportedChainId.GNOSIS_CHAIN]: 'gnosis-mainnet',
@@ -19,4 +19,6 @@ export const GOLD_RUSH_CLIENT_NETWORK_MAPPING: Record<
   [SupportedChainId.SEPOLIA]: 'eth-sepolia',
   [SupportedChainId.LENS]: 'lens-mainnet',
   [SupportedChainId.BNB]: 'bsc-mainnet',
+  [SupportedChainId.LINEA]: 'linea-mainnet',
+  [SupportedChainId.PLASMA]: null,
 };
